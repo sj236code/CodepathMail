4521ef535b38e6e3e604d18d6960463ef9eb9d77
saanyay <saanyajauhri@gmail.com>
2026-09-23 19:39:22 -0400

    add load button

---
+++ app/src/main/java/com/example/codepathmail/EmailAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.codepathmail
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class EmailAdapter(private val emails: List<Email>) :
+    RecyclerView.Adapter<EmailAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val senderTextView: TextView
+        val titleTextView: TextView
+        val summaryTextView: TextView
+
+        init {
+            senderTextView = itemView.findViewById(R.id.senderTv)
+            titleTextView = itemView.findViewById(R.id.titleTv)
+            summaryTextView = itemView.findViewById(R.id.summaryTv)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+
+        val emailView = inflater.inflate(
+            R.layout.email_item,
+            parent,
+            false
+        )
+
+        return ViewHolder(emailView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val email = emails[position]
+
+        holder.senderTextView.text = email.sender
+        holder.titleTextView.text = email.title
+        holder.summaryTextView.text = email.summary
+    }
+
+    override fun getItemCount(): Int {
+        return emails.size
+    }
+}
--- app/src/main/java/com/example/codepathmail/MainActivity.kt
+++ app/src/main/java/com/example/codepathmail/MainActivity.kt
@@ -2,12 +2,43 @@ package com.example.codepathmail
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var emails: MutableList<Email>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+
+        // Find the RecyclerView
+        val emailsRv = findViewById<RecyclerView>(R.id.emailsRv)
+
+        // Get the first 10 emails
+        emails = EmailFetcher.getEmails()
+
+        // Create the adapter
+        val adapter = EmailAdapter(emails)
+
+        // Connect the adapter to the RecyclerView
+        emailsRv.adapter = adapter
+
+        // Display emails vertically
+        emailsRv.layoutManager = LinearLayoutManager(this)
+
+        findViewById<Button>(R.id.loadMoreBtn).setOnClickListener {
+
+            // Get the next 5 emails
+            val newEmails = EmailFetcher.getNext5Emails()
+
+            // Add them to our current list
+            emails.addAll(newEmails)
+
+            // Tell the RecyclerView that the data changed
+            adapter.notifyDataSetChanged()
+        }
     }
 }
